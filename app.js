8be1e19cdf83fb2da9dc47bc0648229f75dd42e6
unknown <amoghveluri19@gmail.com>
2020-07-07 21:23:46 +0530

    Made the menu bar for mobile

---
+++ app.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+	 
+	burger.addEventListener('click',() =>{
+		nav.classList.toggle('nav-active');
+		navLinks.forEach((link,index) =>{
+			if(link.style.animation){ link.style.animation='';}
+			else{link.style.animation='navLinkFade 0.5 ease forwards ${index/0.5}s';}
+		})
+	});
+}
+
+try {
+	navSlide();
+} catch (e) {
+	//Catch Statement
+}
